a6ef17cf36f9888ed7003886f85778190931de1d
rs2387 <rs2387@STUDENT-LT2508.localdomain>
2026-01-27 11:14:02 +0000

    Finished Task1C and fixed Task1B

--- Task1C.py
+++ Task1C.py
@@ -5,5 +5,7 @@ from floodsystem.geo import stations_within_radius
 stations = build_station_list()
 centre = (52.2053, 0.1218)
 
-print(((stations_within_radius(stations, centre, 10)).sort()))
+stations_in_radius = stations_within_radius(stations, centre, 10)
+
+print(sorted(stations_in_radius))
 
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -12,13 +12,12 @@ import math
 earth_radius = 6371
 
 def haversine(p, phi1, lambda1):
-    phi2, lambda2 = p[0], p[1]
-    deltaphi = phi2 - phi1
-    deltalambda = lambda2 - lambda1
-    hav = (1 - math.cos(deltaphi)+ math.cos(phi1)*math.cos(phi2)*(1-math.cos(deltalambda)))/2
+    phi2, lambda2 = p[0]*math.pi/180, p[1]*math.pi/180
+    deltaphi = phi2 - phi1*math.pi/180
+    deltalambda = lambda2 - lambda1*math.pi/180
+    hav = (1 - math.cos(deltaphi)+ math.cos(phi1*math.pi/180)*math.cos(phi2)*(1-math.cos(deltalambda)))/2
     theta = 2*math.asin((hav)**0.5)
-    rad = theta*math.pi/180
-    return earth_radius*rad
+    return earth_radius*theta
 
 
 
@@ -33,10 +32,12 @@ def stations_by_distance(stations, p):
     return sorted_total
 
 def stations_within_radius(stations, centre, r):
-    total = []
+    y = []
     for station in stations:
-        if haversine(centre, station.coord[0], station.coord[1]) < r:
-            total.append(station.name)
+        x = float(haversine(centre, station.coord[0], station.coord[1]))
+        if x < r:
+            y.append(station.name)
 
-    return total
+
+    return y
 
