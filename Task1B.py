9b57a88932ab6d1204ed4f66d36c1df73083d2a1
rs2387 <rs2387@STUDENT-LT2508.localdomain>
2026-01-27 09:38:59 +0000

    Completed Task1B

--- Task1B.py
+++ Task1B.py
@@ -1,2 +1,14 @@
 
 from floodsystem.stationdata import build_station_list
+
+from floodsystem.geo import stations_by_distance
+
+p = (52.2053, 0.1218)
+
+stations = build_station_list()
+
+sorted_total = stations_by_distance(stations, p)
+
+print(sorted_total[:10])
+
+print(sorted_total[-10:])
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,28 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+import math
+
+r = 6371
+
+def haversine(p, phi1, lambda1):
+    phi2, lambda2 = p[0], p[1]
+    deltaphi = phi2 - phi1
+    deltalambda = lambda2 - lambda1
+    hav = (1 - math.cos(deltaphi)+ math.cos(phi1)*math.cos(phi2)*(1-math.cos(deltalambda)))/2
+    theta = 2*math.asin((hav)**0.5)
+    rad = theta*math.pi/180
+    return r*rad
+
+
+
+def stations_by_distance(stations, p):
+    total = []
+    for station in stations:
+        total.append((station.name, station.town, haversine(p, station.coord[0], station.coord[1])))
+
+    sorted_total = sorted_by_key(total, 2)
+
+
+    return sorted_total
+
