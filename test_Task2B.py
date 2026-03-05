16518ef125a2c2a4f91b8bb5be959d3e24968f18
ddnp2 <ddnp2@cam.ac.uk>
2026-03-05 09:36:35 +0000

    test indicated error with 2B final loop

--- test_Task2B.py
+++ test_Task2B.py
@@ -8,16 +8,27 @@ none = MonitoringStation("station_id_none", "measure_id_none", "label_none", (0,
 
 zeroes = MonitoringStation("station_id_zeroes", "measure_id_zeroes", "label_zeroes", (0,0), (0,0), "river_zeroes", "town_zeroes")
 
+zeroes.latest_level = 0
+
 typical1 = MonitoringStation("station_id_typical1", "measure_id_typical1", ["label_typical1", "label_typical1"], (2.5,5), (6,4.2), "river_typical1", "town_typical1")
+
+typical1.latest_level = 5
 
 typical2 = MonitoringStation("station_id_typical2", "measure_id_typical2", ["label_typical2", "label_typical2"], (2.5,5), (2,5.3), "river_typical2", "town_typical2")
 
-typical3 = MonitoringStation("station_id_typical3", "measure_id_typical3", ["label_typical3", "label_typical3"], (0,6), (9,0), "river_typical3", "town_typical3")
+typical2.latest_level = 1
+
+typical3 = MonitoringStation("station_id_typical3", "measure_id_typical3", "label_typical3", (0,6), (0,9), "river_typical3", "town_typical3")
+
+typical3.latest_level = 30
 
 case = [none, zeroes, typical1, typical2, typical3]
 
 tol = 0.8
 total = stations_level_over_threshold(case, tol)
+
+print(total)
+
 for x in range(len(total)-1,0, -1):
     print(total[x])
 
