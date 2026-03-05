ea0f0b7d358af0e3f57f654810ee2aee6e7dddc0
ddnp2 <ddnp2@cam.ac.uk>
2026-03-05 01:14:48 +0000

    first try at coming up with cases and iterating through

--- test_Task2B.py
+++ test_Task2B.py
@@ -3,13 +3,21 @@ from floodsystem.flood import stations_level_over_threshold
 
 from floodsystem.station import MonitoringStation
 
-stations = [MonitoringStation("station_id", "measure_id", "label", (0,0), (0,0), "river", "town")]
+nones = [MonitoringStation("station_id", "measure_id", "label", (0,0), (0,0), "river", "town")]
 
-tol = 0.8
-total = stations_level_over_threshold(stations, tol)
+zeroes = [MonitoringStation("station_id", "measure_id", "label", (0,0), (0,0), "river", "town")]
 
-for x in range(len(total)-1,0, -1):
-    print(total[x])
+typical = [MonitoringStation("station_id", "measure_id", "label", (0,0), (0,0), "river", "town")]
+
+cases = [nones, zeroes, typical]
+
+for case in cases:
+
+    tol = 0.8
+    total = stations_level_over_threshold(case, tol)
+
+    for x in range(len(total)-1,0, -1):
+        print(total[x])
 
 
 
