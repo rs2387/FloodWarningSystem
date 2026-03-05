0d64782869f5962d11f9bcbafc62b8b1d89eb664
ddnp2 <ddnp2@cam.ac.uk>
2026-03-05 01:24:57 +0000

    gained understanding of typical database values to inform case selection

---
+++ database_values_generation.py
@@ -0,0 +1,11 @@
+
+from floodsystem.station import MonitoringStation
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+stations = build_station_list()
+
+update_water_levels(stations)
+
+for station in stations[100:120]:
+    print(station)
